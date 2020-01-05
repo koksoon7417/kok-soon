f881424f17ed2d8ac512b5c094b460688c75c472
Kok Soon <koksoon7417@gmail.com>
2020-01-05 18:00:20 +0800

    link-preview

--- src/App.js
+++ src/App.js
@@ -12,6 +12,12 @@ class App extends Component {
   render() {
     return (
       <div className="dev-landing-page">
+        <meta prefix="og: http://ogp.me/ns#" property="og:title" content="My Portfolio" />
+        <meta prefix="og: http://ogp.me/ns#" property="og:type" content="video.movie" />
+        <meta prefix="og: http://ogp.me/ns#" property="og:image" content="http://www.frogsonwheels.net/wp-content/uploads/2013/11/Linker-Logo-300x300px-01.png" />
+        <meta prefix="og: http://ogp.me/ns#" property="og:url" content="https://koksoon7417.github.io/kok-soon/" />
+        <meta prefix="og: http://ogp.me/ns#" property="og:description" content="This is my portfolio." />
+
         <ThemeSwitcher>
           <Browser except firefox>
             <ParallaxBackground />
